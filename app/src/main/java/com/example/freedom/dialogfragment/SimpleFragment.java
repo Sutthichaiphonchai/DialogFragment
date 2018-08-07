3fef14e2c8aa3fd76b7033de1f6dc2bb8b08003b
cloudy <sutthichaiphonchai@gmail.com>
2018-08-07 20:30:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/freedom/dialogfragment/SimpleFragment.java
@@ -0,0 +1,77 @@
+package com.example.freedom.dialogfragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class SimpleFragment extends DialogFragment implements AwesomeDialogFragment.OnDialogListener {
+    private static String TAG_DIALOG = "dialog";
+
+    private Button btnShowDialog;
+
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
+    }
+
+    public SimpleFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_simple, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnShowDialog = (Button) view.findViewById(R.id.btn_show_dialog);
+        btnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onShowDialogButtonClick();
+            }
+        });
+
+        if (savedInstanceState == null) {
+            addNestedFragment();
+        }
+    }
+
+    private void onShowDialogButtonClick() {
+        AwesomeDialogFragment fragment = new AwesomeDialogFragment.Builder()
+                .setMessage(R.string.sample_dialog_message)
+                .setPosition(R.string.ok)
+                .setNegative(R.string.cancel)
+                .build();
+        fragment.show(getChildFragmentManager(), TAG_DIALOG);
+    }
+
+    private void addNestedFragment() {
+        String tag = NestedFragment.class.getSimpleName();
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.layout_fragment_container, NestedFragment.newInstance(), tag)
+                .addToBackStack(tag)
+                .commit();
+    }
+
+    @Override
+    public void onPositiveButtonClick() {
+        Toast.makeText(getContext(), R.string.sample_dialog_confirm, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNegativeButtonClick() {
+        Toast.makeText(getContext(), R.string.sample_dialog_cancel, Toast.LENGTH_SHORT).show();
+    }
+}
